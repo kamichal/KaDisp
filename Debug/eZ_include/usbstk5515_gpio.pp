710bef9354ac97b37c0084fe9122b559bb72692a
kamichal <michalskaczmarczyk@gmail.com>
2015-02-13 03:16:54 +0100

    Fixed few things. Renamed some stuff. Added a test, removed other as well.

--- Debug/eZ_include/usbstk5515_gpio.pp
+++
@@ -1,9 +0,0 @@
-# FIXED
-
-eZ_include/usbstk5515_gpio.obj: ../eZ_include/usbstk5515_gpio.c
-eZ_include/usbstk5515_gpio.obj: ../eZ_include/usbstk5515_gpio.h
-eZ_include/usbstk5515_gpio.obj: ../eZ_include/usbstk5515.h
-
-../eZ_include/usbstk5515_gpio.c: 
-../eZ_include/usbstk5515_gpio.h: 
-../eZ_include/usbstk5515.h: 
--- Debug/kadisp/font_8x6_philippe.pp
+++
@@ -1,7 +0,0 @@
-# FIXED
-
-kadisp/font_8x6_philippe.obj: ../kadisp/font_8x6_philippe.c
-kadisp/font_8x6_philippe.obj: ../kadisp/font_8x6_philippe.h
-
-../kadisp/font_8x6_philippe.c: 
-../kadisp/font_8x6_philippe.h: 
